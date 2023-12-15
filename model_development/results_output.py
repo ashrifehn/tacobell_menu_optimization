99cb9c82361bd47b94b0221e695b2b032d35c310
ashrif_n1@denison.edu <nashrifeh@outlook.com>
2023-12-15 14:40:10 -0500

    small changes

--- model_development/results_output.py
+++ model_development/results_output.py
@@ -10,7 +10,7 @@ def n_counter(df):
     """This function removes n number of empyty rows in the final dataset that contain the results of each model"""
 
     count = 0
-    for col in df.columns[3:]:
+    for col in df.columns[4:]:
         if '' in df[col].values:
             place_hold = col
             break
@@ -77,7 +77,7 @@ def dataframe_to_docxTable(df, m):
 ## All create dataframe for results
 
 
-def output_to_table_no_sections(m, decison_var, menu_data, gurobi=gp):
+def output_to_table(m, decison_var, menu_data, gurobi=gp):
 #Defining variables
     price = menu_data['price'].tolist()
     menu_item = menu_data['item_name'].tolist()
@@ -99,119 +99,30 @@ def output_to_table_no_sections(m, decison_var, menu_data, gurobi=gp):
     iron = menu_data['Iron (mg)'].tolist()
     Potassium = menu_data['Potassium (mg)'].tolist()
 
-
+    nutri_decison_var = []
+    nutri_decison_var_value = []
     menu_items = []
+    section_var = []
     servings = []
     nutrients = ["Calories", "Protein (g)", "Total Carbohydrate (g)", "Dietary Fiber (g)", "Total Fat (g)", "Saturated Fat (g)",
                  "Trans Fat (g)", "Cholesterol (mg)", "Sodium (mg)", "Sugars (g)", "Potassium (mg)", "Iron (mg)",
                    "Calcium (mg)"]
     nutrient_values = []
     RDA_per_meal = [2400//3, 56//3, 130//3, 28//3, 93//3, 26//3, 0//3, 300//3, 2300//3, 10//3, 3400//3, 8//3,1000//3] 
+
+
+    meal_price =[round(m.objVal, 2)]
 
     # Adding items and their servings to the lists
     for var in m.getVars():
-        if var.x > 0:
+        if var.x > 0 and var.Varname in menu_item:
+            item_section = menu_data.loc[menu_data['item_name'] == var.Varname, 'menu_section'].iloc[0]
+            section_var.append(item_section)
             menu_items.append(var.VarName)
             servings.append(round(var.X, 2))
-    
-     # Adding nutrient values to the nutrient_values list
-    nutrient_values.append(gp.quicksum(round(calories[i]*decison_var[i].x, 2) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(protein[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(totalCarbohydratets[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(dietaryFiber[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(totfat[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(statFat[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(transFat[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(cholesterol[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(sodium[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(sugars[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(Potassium[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(iron[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(Calcium[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-
-    # Creating the DataFrame
-    df = pd.DataFrame({
-        "Menu": menu_items + [""]*(len(nutrients)+1),
-        "Servings": servings + [""]*(len(nutrients)+1),
-        "Nutrient": [""]*(len(menu_items)+1) + nutrients,
-        "Values": [""]*(len(menu_items)+1) + nutrient_values,
-        "Recommended Minimum Per Meal (%)": [""]*(len(menu_items)+1) + RDA_per_meal
-    })
-
-    n = n_counter(df) # Value changes as needed
-    df["Nutrient"] = df["Nutrient"].shift(-n)
-    df["Values"] = df["Values"].shift(-n)
-    df["Recommended Minimum Per Meal (%)"] = df["Recommended Minimum Per Meal (%)"].shift(-n)
-
-    
-    df = df.replace({None: ''})
-    df = df.iloc[:-(n)]
-    
-    df['Values'] = pd.to_numeric(df['Values'], errors='coerce')
-    df['Recommended Minimum Per Meal (%)'] =  [calculate_percentage_of_recommended(model, rec) for model, rec in zip(df['Values'], RDA_per_meal)]
-
-
-    dataframe_to_docxTable(df,m)
-
-    return df
-
-
-
-
-
-def output_to_table_sections(m, decison_var, menu_data, gurobi=gp):
-#Defining variables
-    price = menu_data['price'].tolist()
-    menu_item = menu_data['item_name'].tolist()
-    section_name = menu_data['menu_section'].tolist()
-
-    calories = menu_data['Calories'].tolist()
-    protein = menu_data['Protein (g)'].tolist()
-    totalCarbohydratets = menu_data['Total Carbohydrates (g)'].tolist()
-    dietaryFiber = menu_data['Dietary Fiber (g)'].tolist()
-    totfat = menu_data['Total Fat (g)'].tolist()
-    statFat = menu_data['Saturated Fat (g)'].tolist()
-    transFat = menu_data['Trans Fat (g)'].tolist()
-    cholesterol = menu_data['Cholesterol (mg)'].tolist()
-    sodium = menu_data['Sodium (mg)'].tolist()
-    sugars = menu_data['Total Sugars (g)'].tolist()
-
-    vitamin_d = menu_data['Vitamin D (mcg)'].tolist()
-    Calcium = menu_data['Calcium (mg)'].tolist()
-    iron = menu_data['Iron (mg)'].tolist()
-    Potassium = menu_data['Potassium (mg)'].tolist()
-
-
-    
-    menu_items = []
-    servings = []
-    nutrients = ["Calories", "Protein (g)", "Total Carbohydrate (g)", "Dietary Fiber (g)", "Total Fat (g)", "Saturated Fat (g)",
-                 "Trans Fat (g)", "Cholesterol (mg)", "Sodium (mg)", "Sugars (g)", "Potassium (mg)", "Iron (mg)",
-                   "Calcium (mg)"]
-    
-    RDA_per_meal = [2400//3, 56//3, 130//3, 28//3, 93//3, 26//3, 0//3, 300//3, 2300//3, 10//3, 3400//3, 8//3,1000//3] 
-
-    nutrient_values = []
-    
-    nutri_decison_var = []
-    nutri_decison_var_value = []
-
-    section_var = []
-    section_var_value = []
-
-# Adding items and their servings to the lists
-    for var in m.getVars():
-        if var.x > 0:
-            if var.VarName in nutrients:
-                nutri_decison_var.append(var.VarName)
-                nutri_decison_var_value.append(round(var.X, 2))
-            elif var.varName[6:] in section_name:
-                section_var.append(var.VarName[6:])
-                section_var_value.append(var.X)
-            else:
-                menu_items.append(var.VarName)
-                servings.append(round(var.X, 2))
-    
+        if var.VarName in nutrients:
+            nutri_decison_var.append(var.VarName)
+            nutri_decison_var_value.append(round(var.X, 2))
     
      # Adding nutrient values to the nutrient_values list
     nutrient_values.append(gp.quicksum(round(calories[i]*decison_var[i].x, 2) for i in range(len(menu_data))).getValue())
@@ -229,8 +140,13 @@ def output_to_table_sections(m, decison_var, menu_data, gurobi=gp):
     nutrient_values.append(gp.quicksum(round(Calcium[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
 
     df = pd.DataFrame({
-        "Menu": menu_items + nutri_decison_var+ [""]*((len(nutrients))-len(nutri_decison_var)),
-        "Servings": servings + nutri_decison_var_value+ [""]*(len(nutrients)-len(nutri_decison_var_value)),
+        # "Menu": menu_items + nutri_decison_var+ [""]*((len(nutrients))-len(nutri_decison_var)),
+
+        "Menu": menu_items + [""]*((len(nutrients))),
+        "Servings": servings + [""]*(len(nutrients)),
+        "Meal Price": meal_price + [""]*(len(nutrients)+len(menu_items)-1),
+        
+        # "Servings": servings + nutri_decison_var_value+ [""]*(len(nutrients)-len(nutri_decison_var_value)),
         "Section Name": section_var + [""]*(len(nutrients)),
         "Nutrient": [""]*(len(menu_items)) + nutrients,
         "Values": [""]*(len(menu_items)) + nutrient_values,
@@ -242,10 +158,10 @@ def output_to_table_sections(m, decison_var, menu_data, gurobi=gp):
     df["Values"] = df["Values"].shift(-n)
     df["Recommended Minimum Per Meal (%)"] = df["Recommended Minimum Per Meal (%)"].shift(-n)
 
-    
+
     df = df.replace({None: ''})
     df = df.iloc[:-(n)]
-    
+
     df['Values'] = pd.to_numeric(df['Values'], errors='coerce')
     df['Recommended Minimum Per Meal (%)'] =  [calculate_percentage_of_recommended(model, rec) for model, rec in zip(df['Values'], RDA_per_meal)]
 
@@ -253,100 +169,3 @@ def output_to_table_sections(m, decison_var, menu_data, gurobi=gp):
     dataframe_to_docxTable(df,m)
 
     return df
-
-
-def output_to_table_sections_cal_decision_var(m, decison_var, menu_data, gurobi=gp):
-#Defining variables
-    price = menu_data['price'].tolist()
-    menu_item = menu_data['item_name'].tolist()
-    section_name = menu_data['menu_section'].tolist()
-
-    calories = menu_data['Calories'].tolist()
-    protein = menu_data['Protein (g)'].tolist()
-    totalCarbohydratets = menu_data['Total Carbohydrates (g)'].tolist()
-    dietaryFiber = menu_data['Dietary Fiber (g)'].tolist()
-    totfat = menu_data['Total Fat (g)'].tolist()
-    statFat = menu_data['Saturated Fat (g)'].tolist()
-    transFat = menu_data['Trans Fat (g)'].tolist()
-    cholesterol = menu_data['Cholesterol (mg)'].tolist()
-    sodium = menu_data['Sodium (mg)'].tolist()
-    sugars = menu_data['Total Sugars (g)'].tolist()
-
-    vitamin_d = menu_data['Vitamin D (mcg)'].tolist()
-    Calcium = menu_data['Calcium (mg)'].tolist()
-    iron = menu_data['Iron (mg)'].tolist()
-    Potassium = menu_data['Potassium (mg)'].tolist()
-
-
-    
-    menu_items = []
-    servings = []
-    nutrients = ["Calories", "Protein (g)", "Total Carbohydrate (g)", "Dietary Fiber (g)", "Total Fat (g)", "Saturated Fat (g)",
-                 "Trans Fat (g)", "Cholesterol (mg)", "Sodium (mg)", "Sugars (g)", "Potassium (mg)", "Iron (mg)",
-                   "Calcium (mg)"]
-    
-    RDA_per_meal = [2400//3, 56//3, 130//3, 28//3, 93//3, 26//3, 0//3, 300//3, 2300//3, 10//3, 3400//3, 8//3,1000//3] 
-
-    nutrient_values = []
-    
-    nutri_decison_var = []
-    nutri_decison_var_value = []
-
-    section_var = []
-    section_var_value = []
-
-# Adding items and their servings to the lists
-    for var in m.getVars():
-        if var.x > 0:
-            if var.VarName in nutrients:
-                nutri_decison_var.append(var.VarName)
-                nutri_decison_var_value.append(round(var.X, 2))
-            elif var.varName[6:] in section_name:
-                section_var.append(var.VarName[6:])
-                section_var_value.append(var.X)
-            else:
-                menu_items.append(var.VarName)
-                servings.append(round(var.X, 2))
-    
-    
-     # Adding nutrient values to the nutrient_values list
-    nutrient_values.append(gp.quicksum(round(calories[i]*decison_var[i].x, 2) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(protein[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(totalCarbohydratets[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(dietaryFiber[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(totfat[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(statFat[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(transFat[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(cholesterol[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(sodium[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(sugars[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(Potassium[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(iron[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-    nutrient_values.append(gp.quicksum(round(Calcium[i]*decison_var[i].x) for i in range(len(menu_data))).getValue())
-
-    df = pd.DataFrame({
-        "Menu": menu_items + nutri_decison_var+ [""]*((len(nutrients))-len(nutri_decison_var)),
-        "Servings": servings + nutri_decison_var_value+ [""]*(len(nutrients)-len(nutri_decison_var_value)),
-        "Section Name": section_var + [""]*(len(nutrients)+2),
-        "Nutrient": [""]*(len(menu_items)) + nutrients,
-        "Values": [""]*(len(menu_items)) + nutrient_values,
-        "Recommended Minimum Per Meal (%)": [""]*(len(menu_items)) + RDA_per_meal
-    })
-
-    n = n_counter(df) # Value changes as needed
-    df["Nutrient"] = df["Nutrient"].shift(-n)
-    df["Values"] = df["Values"].shift(-n)
-    df["Recommended Minimum Per Meal (%)"] = df["Recommended Minimum Per Meal (%)"].shift(-n)
-
-    
-    df = df.replace({None: ''})
-    df = df.iloc[:-(n)]
-    
-    df['Values'] = pd.to_numeric(df['Values'], errors='coerce')
-    df['Recommended Minimum Per Meal (%)'] =  [calculate_percentage_of_recommended(model, rec) for model, rec in zip(df['Values'], RDA_per_meal)]
-
-
-    dataframe_to_docxTable(df,m)
-
-    return df
-    
